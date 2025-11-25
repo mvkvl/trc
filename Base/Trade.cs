7b758c8cea4fdad9650b910f393d60be081545f6
mvkvl <mvkvl@local>
2025-11-25 12:42:38 +0300

    wip

---
+++ Base/Trade.cs
@@ -0,0 +1,16 @@
+﻿namespace Base
+{
+    public class Trade
+    {
+        #region ===== Fields =====
+        
+        public required decimal  Price;
+        public required decimal  Volume;
+        public required string   SecurityCode;
+        public required string   ClassCode;
+        public required string   Portfolio;
+        public required DateTime Time;
+        
+        #endregion
+    }
+}
