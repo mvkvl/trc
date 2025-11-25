8a6ac1271e6f3f48e3f6ef555928673fa9e0c9a6
mvkvl <mvkvl@local>
2025-11-25 12:28:25 +0300

    wip

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using Base;
+
+
+# region ==== initialization ====
+
+var startLevel = ConsoleUtil.ReadInput("enter initial level");
+var stepSize   = ConsoleUtil.ReadInput("enter stepSize");
+var stepsCount = ConsoleUtil.ReadInput("enter number of steps");
+
+# endregion
+
+# region ==== tests ====
+
+//Console.WriteLine("\nTest non-uniform short network:");
+//TestNonUniformNetwork(Side.Sell, startLevel, stepSize, stepsCount);
+
+//Console.WriteLine("\nTest non-uniform long network:");
+//TestNonUniformNetwork(Side.Buy, startLevel, stepSize, stepsCount);
+
+//Console.WriteLine("\nTest uniform short network:");
+//TestUniformNetwork(Side.Sell, startLevel, stepSize, stepsCount);
+
+Console.WriteLine("\nTest uniform long network:");
+TestUniformNetwork(Side.Buy, startLevel, stepSize, stepsCount);
+
+# endregion
+
+# region === test methods ====
+static void TestNonUniformNetwork(Side side, decimal sl, decimal ss, decimal sc)
+{
+    var net = new Network(side);
+    var p = sl;
+    for (int i = 0; i < sc; i++)
+    {
+        net.Add(p);
+        if (side == Side.Sell)
+        {
+            p -= ss;
+            if (p < 0)
+            {
+                p = 0;
+            }
+        } else
+        {
+            p += ss;
+        }
+    }
+    Console.WriteLine(net.ToString());
+}
+
+static void TestUniformNetwork(Side side, decimal sl, decimal ss, decimal sc)
+{
+    var net = new Network(side, ss);
+    net.Add(sl);
+    for (int i = 1; i < sc; i++)
+    {
+        net.Add();
+    }
+    Console.WriteLine(net.ToString());
+}
+
+# endregion
+
---
+++ Util/Util.cs
@@ -0,0 +1,21 @@
+﻿namespace Util
+{
+    internal class ConsoleUtil
+    {
+        public static decimal ReadInput(string q)
+        {
+            Console.Write(q + ": ");
+            var s = Console.ReadLine();
+            try
+            {
+                return decimal.Parse(s);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("input error: " + e.Message);
+                System.Environment.Exit(1);
+            }
+            return 0;
+        }
+    }
+}
