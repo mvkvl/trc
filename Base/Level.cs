27badd64fe03afa0bf9b48c4b87b6ac4a957099c
mvkvl <mvkvl@local>
2025-11-25 11:09:47 +0300

    wip

--- Base/Level.cs
+++ Base/Level.cs
@@ -11,6 +11,14 @@ namespace Base
     public class Level
     {
 
+        /// <summary>
+        /// Цена
+        /// </summary>
+        public decimal Price = 0;
+
+
+
+
     }
     #endregion
 
