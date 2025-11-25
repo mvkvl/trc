53deb6bb46a47f03ac3ae50caf6e8aab780abb03
mvkvl <mvkvl@local>
2025-11-25 12:55:55 +0300

    wip

--- Base/Trade.cs
+++ Base/Trade.cs
@@ -5,12 +5,29 @@
         #region ===== Fields =====
         
         public required decimal  Price        = 0;
-        public required decimal  Volume       = 0;
         public required string   SecurityCode = "";
         public required string   ClassCode    = "";
         public required string   Portfolio    = "";
         public required DateTime Time         = DateTime.MinValue;
-        
+
+        #endregion
+        #region ===== Properties =====
+        private decimal _volume = 0;
+        public decimal Volume
+        {
+            get
+            {
+                return _volume;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidDataException("volume can't be negative");
+                }
+                _volume = value;
+            }
+        }
         #endregion
     }
 }
