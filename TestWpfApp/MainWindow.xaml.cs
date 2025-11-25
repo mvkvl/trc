59c4c2af5a0ac68ef7c8ea8564922d023b0cc546
mvkvl <mvkvl@local>
2025-11-25 13:07:15 +0300

    wip

---
+++ TestWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TextBlock tb = new TextBlock();
+            tb.TextWrapping = TextWrapping.Wrap;
+            tb.VerticalAlignment = VerticalAlignment.Top;
+            tb.Margin = new Thickness(10);
+            tb.Inlines.Add("An example on ");
+            tb.Inlines.Add(new Run("the TextBlock control ") { FontWeight = FontWeights.Bold });
+            tb.Inlines.Add("using ");
+            tb.Inlines.Add(new Run("inline ") { FontStyle = FontStyles.Italic });
+            tb.Inlines.Add(new Run("text formatting ") { Foreground = Brushes.Blue });
+            tb.Inlines.Add("from ");
+            tb.Inlines.Add(new Run("Code-Behind") { TextDecorations = TextDecorations.Underline });
+            tb.Inlines.Add(".");
+            grid1.Children.Add(tb);
+
+        }
+
+        private void HyperlinkRequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+
+        }
+        private void TextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            txtStatus.Text = "Selection starts at character #" + textBox.SelectionStart + Environment.NewLine;
+            txtStatus.Text += "Selection is " + textBox.SelectionLength + " character(s) long" + Environment.NewLine;
+            txtStatus.Text += "Selected text: '" + textBox.SelectedText + "'";
+        }
+        private void HelloWorldButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, world!");
+        }
+        private void AcceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Действие выполнено");
+        }
+        private void EscButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // закрытие окна
+        }
+
+    }
+}
